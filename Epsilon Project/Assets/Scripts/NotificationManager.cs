5af177a6b2d5ec89757d5f0ad6e6876181ab5d6c
Benjamin Chariou <chariou.benjamin@gmail.com>
2021-04-28 16:50:45 +0200

    Timer dans l'application °+ notif

---
+++ Epsilon Project/Assets/Scripts/NotificationManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Notifications.Android;
+
+public class NotificationManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var defaultNotificationChannel = new AndroidNotificationChannel()
+        {
+            Id = "default_channel",
+            Name = "Default Channel",
+            Description = "For generic notifications",
+            Importance = Importance.Default
+        };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(defaultNotificationChannel);
+
+        
+
+      
+
+        AndroidNotificationCenter.NotificationReceivedCallback receivedNotificationHandler = delegate (AndroidNotificationIntentData data)
+        {
+            var msg = "Notification received : " + data.Id + "\n";
+            msg += "\n Notification received ";
+            msg += "\n .Title " + data.Notification.Text;
+            msg += "\n .Channel: " + data.Channel;
+            Debug.Log(msg);
+        };
+
+        AndroidNotificationCenter.OnNotificationReceived += receivedNotificationHandler;
+
+        var notificationIntentData = AndroidNotificationCenter.GetLastNotificationIntent();
+
+        if(notificationIntentData != null)
+        {
+            Debug.Log("App was opened with notification");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SendNotification(float waitTime)
+    {
+        AndroidNotification notification = new AndroidNotification()
+        {
+            Title = "Test",
+            Text = "This is a test",
+            SmallIcon = "default",
+            LargeIcon = "default",
+            FireTime = System.DateTime.Now.AddSeconds(waitTime),
+        };
+        var identifier = AndroidNotificationCenter.SendNotification(notification, "default_channel");
+    }
+}
---
+++ Epsilon Project/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public NotificationManager notificationScript;
+    public System.DateTime storedTime;
+    public System.DateTime currentTime;
+    public System.DateTime timeToReach;
+    bool currentlyWaiting = false;
+    //public float timeValue = 60;
+    //public Text timerText;
+
+    private void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        currentTime = System.DateTime.UtcNow.ToLocalTime();
+        if (currentTime >= timeToReach && currentlyWaiting == true)
+        {
+            Debug.Log("The wait is over");
+            currentlyWaiting = false;
+        }
+        //Calcule le temps en temps réél quand l'appli est ouverte
+        //if(timeValue > 0) { 
+        //timeValue -= Time.deltaTime;
+        //}
+        //else
+        //{
+        //    timeValue = 0;
+        //    Debug.Log("The wait is finally over");
+        //}
+
+        //DisplayTime(timeValue);
+
+    }
+
+    public void StartWaiting(float secondsToWait)
+    {
+        storedTime = System.DateTime.UtcNow.ToLocalTime();
+        Debug.Log("The wait started at" + storedTime.ToString("HH:mm::ss"));
+        timeToReach = storedTime.AddSeconds(secondsToWait);
+        Debug.Log("You will have to wait until" + timeToReach.ToString("HH:mm:ss"));
+        currentlyWaiting = true;
+        notificationScript.SendNotification(secondsToWait);
+        //timeToReach = storedTime.AddDays(values[0]);
+        //timeToReach = timeToReach.AddHours(values[1]);
+        //timeToReach = timeToReach.AddMinutes(values[2]);
+        //timeToReach = timeToReach.AddSeconds(values[3]);
+    }
+
+    //Permets d'afficher le timer pour du debugging eventuel
+    //void DisplayTime(float timeToDisplay)
+    //{
+    //    if(timeToDisplay < 0)
+    //    {
+    //        timeToDisplay = 0;
+    //    }
+    //    float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+    //    float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+    //    timerText.text = string.Format("{0:00}:{1:00}",minutes,seconds);
+    //}
+}
