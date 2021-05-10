44e211c9e1b73651d473768ce355c98ba2856525
Leo Nimsgern <nimsgernleo@gmail.com>
2021-05-10 11:41:44 +0200

    WIP 1 / Proto 2

---
+++ Epsilon Project/Assets/Script/Leo/SwitchScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    private SceneManager sceneManager;
+    public void SwitchSocialMedia()
+    {
+        SceneManager.LoadScene("SocialMediaMinigame");
+    }
+
+    public void SwitchEMails()
+    {
+        SceneManager.LoadScene("E Mails");
+    }
+
+    public void SwitchGame()
+    {
+        SetNextDIalogue((TextAsset)Resources.Load("Tables\\2a"));
+        SceneManager.LoadScene("Game");
+    }
+
+    public void SetNextDIalogue(TextAsset data)
+    {
+        DialogueManager.Instance.currentDialogueFile = data;
+    }
+}
