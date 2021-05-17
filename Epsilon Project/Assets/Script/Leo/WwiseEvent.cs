91c9694aeff6296fc664fe48c22a356d7103cbde
Leo Nimsgern <nimsgernleo@gmail.com>
2021-05-17 17:29:42 +0200

    ajout d'une soundbank et event Beep test

---
+++ Epsilon Project/Assets/Script/Leo/WwiseEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WwiseEvent : MonoBehaviour
+{
+    public AK.Wwise.Event wwiseEvent;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            wwiseEvent.Post(gameObject);
+        }
+    }
+}
