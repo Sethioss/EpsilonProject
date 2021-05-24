35db3b2e8a058ebabe8c6385cdc54d6c767d4cc8
Leo Nimsgern <nimsgernleo@gmail.com>
2021-05-24 19:34:17 +0200

    intergration post par default

---
+++ Epsilon Project/Assets/Script/Leo/AnimBanner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimBanner : MonoBehaviour
+{
+    public Animator anim;
+    private bool Open = false;
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void ChangeAnim()
+    {
+        if (Open == false)
+        {
+            anim.SetBool("Opning", true);
+            Open = true;
+        }
+        else
+        {
+            anim.SetBool("Opning", false);
+            Open = false;
+        }
+    }
+
+
+}
