97e9d40b25a2da36d8848f6c92fe151d0d88190a
Sethioss <sebastien.guarober@gmail.com>
2021-06-02 16:36:12 +0200

    - Adding options menu functionality with the Dialogue Manager - Changing hour display to show PM/AM hours - Rearranging UI elements to be responsive with the phone's screen size

--- Epsilon Project/Assets/Script/Leo/OptionManager.cs
+++ Epsilon Project/Assets/Script/Leo/OptionManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class OptionMenu : MonoBehaviour
+public class OptionManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
